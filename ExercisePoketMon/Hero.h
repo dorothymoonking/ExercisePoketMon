9901bedc34c09fcf804b49e324736c5c80cb51b5
문선 최 <xsc13@naver.com>
2021-06-16 16:47:34 +0900

    <끌어오기>

--- ExercisePoketMon/Hero.h
+++ ExercisePoketMon/Hero.h
@@ -3,8 +3,10 @@ class Hero
 {
 public:
 	char m_Name[128];
+	int  m_MaxHp;
 	int  m_Hp;
-	int  m_Gold;
+	int AttackRate;
+	int m_Gold = 0;
 
 public:
 	Hero();
@@ -13,5 +15,9 @@ public:
 public:
 	void NewHero(char _Name[128]);
 	void PrintInfo();
+	void SetHP(int _hp);
+	void SetAttack(int _att);
+	void LoadHero(const char* _Name, int _MaxHp, int _hp, int _Gold, int _Att);
+
 };
 
